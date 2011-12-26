050286c5a225495a8975a88745657f7280b30cb1
Adrien Dudouit-Exposito <capitaine.gloumi@gmail.com>
2011-12-26 18:22:59 +0100

    Ajout des fonction pour la création d'un sou corpus

---
+++ script/PHP/PompOView_Corpus/SubCorpus.php
@@ -0,0 +1,36 @@
+<?
+	function subCorpusJSON($jsonOrig,$toErase)
+	{
+		require_once("JSON.php");
+		$json = JSON::serialize(file_get_contents($jsonOrig));
+		$corpus = (array)json_decode($json);
+		
+		// CHANGED Supression des noms de fichier qui ne sont pas dans le sous-corpus
+		$corpus_filename = (array)$corpus["filenames"];
+		foreach($toErase as $key){
+			unset($corpus_filename[$key]);
+		}
+		$corpus["filenames"] = array_values($corpus_filename);
+		
+		// CHANGED Supressions des lignes et colonnes qui n'apartiennent pas au sous-corpus
+		$corpus_scores = (array)$corpus["corpus_scores"];
+		$corpus["corpus_scores"] = array();
+		foreach($toErase as $key){
+			unset($corpus_scores[$key]);
+		}
+		foreach($corpus_scores as $col){
+			foreach($toErase as $key){
+				unset($col[$key]);
+			}
+			$corpus["corpus_scores"][] = array_values($col);
+		}
+		
+		return json_encode($corpus);
+	}
+	function file_set($file,$content){
+		$file = fopen($file,'w');
+		$rez = fputs($file,$content);
+		fclose($file);
+		return $rez;
+	}
+
