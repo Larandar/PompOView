57267443fda87608cd510b1ad2588a371643ce8e
Adrien Dudouit-Exposito <capitaine.gloumi@gmail.com>
2012-02-08 12:02:16 +0100

    Réécriture de PompOView_Corpus => POVCorpus

--- script/php/POVCorpus/POVCorpus.php
+++ script/php/POVCorpus/POVCorpus.php
@@ -1,8 +1,95 @@
 <?php
 	/**
-	 * PompOView a l'échelle d'un corpus
+	 * 
 	 */
 	class POVCorpus {
-		// Actuellement en réécriture pour suivre une architecture mieux délimité
+		/**
+		 * Données primaires
+		 */
+		protected $corpus;
+		
+		protected $cluste;
+		protected $cluste_dist  = "Distance::min";
+		
+		protected $styles;
+		protected $styles_parti = "Partitionneur_Linear";
+		protected $styles_param = null;
+		
+		protected $povc_ui;
+		
+		/**
+		 * Données dérivés
+		 */
+		protected $data;
+		protected $data_list;
+		
+		protected $filenames;
+		protected $subproject;
+		
+		public function __construct() {
+			$this->cluste = new Clustering_Hierarchical(null,$this->cluste_dist);
+			$this->styles = new StyleSet_ColorHSL($this->styles_parti);
+			$this->povc_ui = new POVCorpus_HtmlUI();
+		}
+		
+		public function fromJSON($name) {
+			if (is_file($name)) {
+				$json = file_get_contents($name);
+				$json = Json_Clear::clear($json);
+				$json = Zend_Json::decode($json);
+				
+				$this->corpus = $json;
+				$this->data = $json['corpus_scores'];
+				
+				$this->cluste->setData($this->data);
+				
+				$data_list = ArrayTools_Matrix::triangularValues($json['corpus_scores']);
+				$this->data_list = $data_list;
+				$this->styles->setData($data_list,$this->styles_param);
+				
+				$this->filenames = $json['filenames'];
+			} else {
+				throw new Exception('File "'.$name.'" does not exist !!');
+			}
+		}
+		
+		public function setClustering($cluste_name, $cluste_dist = null) {
+			$this->cluste_dist = (is_null($cluste_dist))?$this->cluste_dist:$cluste_dist;
+			$this->cluste = new $cluste_name($this->data,$this->cluste_dist);
+		}
+		
+		public function setStyleSet($styles,$styles_parti = null,$styles_param = null) {
+			$this->styles_parti = ( is_null($styles_parti) ? $this->styles_parti : $styles_parti );
+			$this->styles_param = ( is_null($styles_param) ? $this->styles_param : $styles_param );
+			
+			$this->styles = new $styles($this->styles_parti,$this->data_list,$this->styles_param);
+		}
+		
+		public function setUI($ui_name) {
+			$this->povc_ui = new $ui_name();
+		}
+		public function makeTable() {
+			return $this->povc_ui->makeTable($this);
+		}
+		public function makeCorpusContent() {
+			return $this->povc_ui->makeCorpusContent($this);
+		}
+		public function makeCorpusStats() {
+			return $this->povc_ui->makeCorpusStats($this);
+		}
+		public function makePOVCorpusForm() {
+			return $this->povc_ui->makePOVCorpusForm($this);
+		}
+		
+		public function getClustering() { return $this->cluste; }
+		public function getStyleSet() { return $this->styles; }
+		
+		public function getData() { return $this->data; }
+		public function getDataList() { return $this->data_list ; }
+		
+		public function getFilenames() { return $this->filenames; }
+		public function getSubProject() { return $this->subproject; }
+		
 	}
+	
 ?>
---
+++ script/php/POVCorpus/POVCorpus_HtmlUI.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	 * Conposante visuel du POVCorpus , pour une nouvelle UI il suffit d'hérité celle-ci est de l'appeller dans le POVCorpus
+	 */
+	class POVCorpus_HtmlUI {
+		
+		public static function makeTable(POVCorpus $povc) {
+			$data = $povc->getData();
+			$style = $povc->getStyleSet();
+			$filenames = $povc->getFilenames();
+			
+			$order = $povc->getClustering()->getOrder();
+			
+			$html = "";
+			$html .= "<table>".PHP_EOL;
+			
+			$html .= "<thead><tr><th></th>";
+			foreach ($order as $o => $i) {
+				$html .= sprintf('<th><abbr title="%s">%s</abbr></th>',$filenames[$i],$o+1);
+			}
+			$html .= "</tr></thead>".PHP_EOL;
+			
+			$html .= "<tbody>".PHP_EOL;
+			foreach ( $order as $o => $i ) {
+				$html .= "<tr>";
+				$html .= sprintf('<th><abbr title="%s">%s</abbr></th>',$filenames[$i],$o+1);
+				foreach ( $order as $j ) {
+					$cour = $data[$i][$j];
+					$html .= sprintf('<td style="%s"><abbr title="%f">%.2f</abbr></td>',$style->getStyleOf($cour),$cour,$cour);
+				}
+				$html .= "</tr>".PHP_EOL;
+			}
+			$html .= "</tbody>".PHP_EOL;
+			$html .= "</table>".PHP_EOL;
+			return $html;
+		}
+		
+		public static function makeCorpusContent(POVCorpus $povc) {
+			$filenames = $povc->getFilenames();
+			$order = $povc->getClustering()->getOrder();
+			
+			$html = "";
+			$html .= '<ul style="list-style:none;">'.PHP_EOL;
+			foreach ($order as $o => $i) {
+				$html .= sprintf('<li><strong>%d</strong>: %s',$o+1,$filenames[$i]).PHP_EOL;
+			}
+			$html .= "</ul>".PHP_EOL;
+			
+			return $html;
+		}
+	}
+	
+?>
