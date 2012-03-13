789eeced2c6da9bbac7d2b5e62ed52f235ca66e4
Adrien Dudouit-Exposito <capitaine.gloumi@gmail.com>
2012-03-13 18:32:31 +0100

    Réidentation

--- script/php/CorpusTools/CorpusTools_SubCorpus.php
+++ script/php/CorpusTools/CorpusTools_SubCorpus.php
@@ -7,33 +7,31 @@
 		 * @param  array toErase : Liste des index de fichier a enlevé du json
 		 * @return JSON Formated String
 		 */
-		public static function subCorpusJSON($jsonOrig, array $toErase)
-			{
-				$json   = Json_Clear::clear(file_get_contents($jsonOrig));
-				$corpus = Zend_Json::decode($json);
+		public static function subCorpusJSON($jsonOrig, array $toErase) {
+			$json   = Json_Clear::clear(file_get_contents($jsonOrig));
+			$corpus = Zend_Json::decode($json);
 				
-				$corpus_filename = (array)$corpus["filenames"];
-				foreach($toErase as $key){
-					unset($corpus_filename[$key]);
-				}
-				$corpus["filenames"] = array_values($corpus_filename);
-				
-				$corpus_scores = (array)$corpus["corpus_scores"];
-				$corpus["corpus_scores"] = array();
-				foreach($toErase as $key){
-					unset($corpus_scores[$key]);
-				}
-				
-				foreach($corpus_scores as $col){
-					foreach($toErase as $key){
-						unset($col[$key]);
-					}
-					$corpus["corpus_scores"][] = array_values($col);
-				}
-				
-				return json_encode($corpus);
+			$corpus_filename = (array)$corpus["filenames"];
+			foreach($toErase as $key){
+				unset($corpus_filename[$key]);
 			}
-		
+			$corpus["filenames"] = array_values($corpus_filename);
+				
+			$corpus_scores = (array)$corpus["corpus_scores"];
+			$corpus["corpus_scores"] = array();
+			foreach($toErase as $key){
+				unset($corpus_scores[$key]);
+			}
+				
+			foreach($corpus_scores as $col){
+				foreach($toErase as $key){
+					unset($col[$key]);
+				}
+				$corpus["corpus_scores"][] = array_values($col);
+			}
+				
+			return json_encode($corpus);
+		}
 	}
 	
 ?>
