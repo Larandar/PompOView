9deda4f117d61cc7ebbeef0914bb3ee0df6df86d
Adrien Dudouit-Exposito <capitaine.gloumi@gmail.com>
2012-03-13 18:25:25 +0100

    [NEW] Classe "Corpus" pour gérer les corpus a partir d'une archive tar.gz

---
+++ script/php/Corpus/Corpus.php
@@ -0,0 +1,81 @@
+<?php
+	/**
+	 * Arboraissance :
+	 * $corpus_name$.tar.gz ->
+	 *     > out.js / out.json / main.json << $manifest
+	 *     > corpus/ > Tout les fichier documents <
+	 *     > log/    > Tout les log de génération <
+	 */
+	class Corpus {
+		
+		protected static $manifest = "out.js";
+		
+		protected $corpus_path;
+		protected $corpus_name;
+		protected $corpus_exte;
+		protected $corpus_stream;
+		
+		protected $json;
+		
+		function __construct($corpus_path) {
+			
+			$this->corpus_path = $corpus_path;
+			
+			if ( is_file($corpus_path.'/'.self::$manifest) ) {
+				$this->corpus_stream = $corpus_path .'/';
+			} else {
+				$this->corpus_stream = 'phar://'.$corpus_path .'/';
+			}
+			
+			
+			if ($this->exist()) {
+				$load = Json_Clear::clear(file_get_contents($this->corpus_stream.self::$manifest));
+				$this->corpus = Zend_Json::decode($load);
+			}
+		}
+		
+		public function exist() {
+			return is_file($this->corpus_stream.self::$manifest);
+		}
+		
+		public function subCorpus($toKeep) {
+			$toErase = array_diff(array_keys($this->corpus["filenames"]),$toKeep);
+			$newjs = CorpusTools_SubCorpus::subCorpusJSON($this->corpus_stream.self::$manifest,$toErase);
+			$this->corpus = Zend_Json::decode($newjs);
+		}
+		
+		public function getScores() {
+			return $this->corpus["corpus_scores"];
+		}
+		
+		public function getFileNames() {
+			return $this->corpus["filenames"];
+		}
+		
+		public function getFileName($id) {
+			return $this->corpus["filenames"][$id];
+		}
+		
+		public function getFile($name) {
+			return file_get_contents($this->corpus_stream.$name);
+		}
+		
+		public function getFileByID($id) {
+			$name = $this->getFileName($id);
+			return $this->getFile($name);
+		}
+		
+		public function getLogName($name_1,$name_2) {
+			$name_1 = preg_replace("#/#", "#", $name_1);
+			$name_2 = preg_replace("#/#", "#", $name_2);
+			return $name_1 .'_x_'. $name_2 .'.png';
+		}
+		
+		public function getLogNameByID($id_1,$id_2) {
+			$name_1 = $this->getFileName($id_1);
+			$name_2 = $this->getFileName($id_2);
+			return $this->getLogName($name_1,$name_2);
+		}
+	}
+	
+?>
---
+++ script/php/Corpus/Corpus_Uploader.php
@@ -0,0 +1,58 @@
+<?php
+	//
+	/**
+	 * 
+	 */
+	class Corpus_Uploader {
+		
+		protected $return;
+		
+		function __construct() {
+			if (!isset($_FILES['corpus_upload']) || $_FILES['corpus_upload']['error'] != 0) {
+				$this->return = array("state" => "error" , "message" => $_FILES['corpus_upload']['error']);
+			} else if ( !preg_match('/(tar\.gz|tgz|targz|zip)$/',$_FILES['corpus_upload']["name"])) {
+				$this->return = array("state" => "error" , "message" => "Only:{tar.gz, targz, tgz, zip} file");
+			} else {
+				$file = $_FILES['corpus_upload'];
+				$newfile = Corpus_Uploader::getFreeName($file["name"]);
+				$move = move_uploaded_file($file["tmp_name"],DATA_DIR.$newfile);
+				$test_corpus = new Corpus(DATA_DIR.$newfile);
+				if ( $test_corpus->exist()) {
+					$this->return = array("state" => "success" , "message" => 'File upload is a success. Save as: <a href="'.DATA_URL.$newfile.'" >'.DATA_URL.$newfile.'</a>');
+				} else {
+					unlink(DATA_DIR.$newfile);
+					$this->return = array("state" => "error" , "message" => "The tar must have a out.js file on is root");
+				}
+			}
+		}
+		
+		public function getReturn($name = null) {
+			if (is_null($name) || !isset($this->return[$name])) {
+				return $this->return;
+			} else {
+				return $this->return[$name];
+			}
+		}
+		
+		
+		public static function getFreeName($name) {
+			
+			if (!is_file(DATA_DIR.$name)) {
+				return $name;
+			} else {
+				$crc = crc32($name.microtime());
+				$replace = '${1}.'.$crc.'${2}';
+				return preg_replace('/(.*)(\.(tar\.gz|targz|zip))/',$replace,$name);
+			}
+		}
+		
+		public static function getForm() {
+			$form = '<form method="post" action="'.URL_AJAX.'corpus-uploader.form.php" enctype="multipart/form-data">';
+			$form .= '<input class="button" type="file" name="corpus_upload" style="width:500px" />';
+			$form .= '<input class="button" type="submit" value="Envoyer" />';
+			$form .= '</form>';
+			return $form;
+		}
+	}
+	
+?>
