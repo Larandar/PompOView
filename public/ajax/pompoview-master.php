3f6c52b6aec4a982800ef618e5cbf557870700c8
Adrien Dudouit-Exposito <capitaine.gloumi@gmail.com>
2012-03-26 16:22:59 +0200

    Update main page

--- public/ajax/pompoview-master.php
+++ public/ajax/pompoview-master.php
@@ -1,19 +1,17 @@
-<h2>PompOView</h2>
+<h2 class="pretty">PompOView</h2>
 <p>
-	<strong>Introduction :</strong> PompOView est un projet universitaire qui a pour but de ... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat condimentum sem, et sagittis erat cursus nec. Proin aliquet auctor odio tempor mattis. Fusce eros leo, convallis eget pellentesque eget, lobortis eget ligula. Aliquam erat volutpat. Curabitur quis velit lacus. Fusce molestie faucibus nisi eu tempus. Donec ac lorem mi, ut placerat justo.
+	<strong>Introduction :</strong> PompOView est un projet universitaire qui a pour but de faire un frontend pour la sortie de l'outils <a href="http://code.google.com/p/pypometre/" onclick="window.open(this.href); return false;"><strong>Pomp-o-metre</strong></a> et ainsi de permettre a la fois la détéction de plagia au sein d'un corpus de documents, soit de permettre detecté la redoncdance de code dans un même corpus (dans le cas de grande équipe de travail par exemple).
 </p>
 <p>
-	Curabitur sed mi eu leo tincidunt tempus. Aenean scelerisque mauris eu magna ornare condimentum. Pellentesque odio purus, consectetur ut consectetur sit amet, imperdiet ac risus. Sed in odio vitae nunc lobortis ultricies ut ac dolor. Nam auctor augue non metus tristique venenatis. Donec egestas tortor orci. Suspendisse mattis ornare cursus. Etiam vestibulum, urna id pharetra congue, enim tortor fermentum mi, nec viverra arcu sem vel velit. Ut turpis risus, aliquet a cursus et, tincidunt sed enim. Ut vel ante arcu, nec vulputate nisi. Pellentesque fermentum semper enim eget rutrum. Suspendisse eu leo ut nunc interdum ornare id ac est. Duis vel dui id ligula tempor tristique.
+	Pour ce faire il faut en premier lieu importé le corpus au format tar.gz formaté <a href="<?php echo SERVER_URL ?>/public/img/TreeView.png"><strong>sous cette arborescence</strong></a>.
 </p>
+<h2 class="pretty"><a style="display:block;" href="" onclick="PompOView.UI.openTab('<?php echo URL_AJAX ; ?>pompoview-newcorpus.php','Nouveaux Corpus'); return false;">Ouvrir un nouveau Corpus</a></h2>
+<h3 class="pretty">A Propos</h3>
 <p>
-	Nulla suscipit hendrerit tellus, vel fermentum odio aliquam in. Suspendisse a quam eget lacus fringilla congue. Donec volutpat orci non nibh tincidunt venenatis. Nulla facilisi. Proin laoreet tristique ligula sit amet vestibulum. Aenean vel diam non metus sollicitudin ullamcorper vitae id nunc. Donec lacinia, erat non placerat imperdiet, mi metus commodo orci, at molestie libero libero a nulla. Ut lacinia ante nisi, eget fermentum urna.
-</p>
-<h3>Commandes</h3>
-<ul class="listing">
-	<li><a class="strong" href="#" onclick="PompOView.UI.openTab('<?php echo URL_AJAX ; ?>pompoview-newcorpus.php','Nouveaux Corpus');">Nouveau Corpus</a> : Importation d'un nouveau corpus</li>
-	<li><a class="strong" href="#" onclick="PompOView.UI.openTab('<?php echo URL_AJAX ; ?>pompoview-diffview.php','{{$1}} ∫ {{$2}}');">Comparaison de documents</a> : Comparaison de document deux à deux.</li>
-</ul>
-<h3>A Propos</h3>
-<p>
-	Quisque id lectus arcu. Praesent sagittis purus in dui euismod congue. Nunc erat nunc, tincidunt a adipiscing vel, semper nec erat. Nam at leo purus, eget sagittis nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia leo et felis interdum fringilla. Nulla iaculis egestas sagittis. Nullam dignissim, ligula eget accumsan rutrum, massa tortor elementum libero, at molestie erat lectus at arcu.
+	<h4>Auteurs</h4>
+	<ul>
+		<li>Adrien Dudouit ( <a href="https://github.com/Larandar" onclick="window.open(this.href); return false;"><strong>Github</strong></a> )</li>
+		<li>Alexandre Lefoupil</li>
+	</ul>
+	Pour obtenir les sources c'est ici <a href="http://larandar.github.com/PompOView/" onclick="window.open(this.href); return false;">http://larandar.github.com/PompOView/</a>
 </p>
